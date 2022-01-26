84e1df5a9f5815abe250e114d73a43207621bb11
alamin937 <mdalamin76812@gmail.com>
2022-01-26 20:52:11 +0600

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var MongoClient = require('mongodb').MongoClient;
 const app = express()
+const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 const cors = require('cors')
 
@@ -25,8 +26,75 @@ async function run() {
       await client.connect();
       const database = client.db("Travel");
       const blogCollection = database.collection("Blog");
+      const userCollection = database.collection("user")
+      const experienceCollection = database.collection("experience")
 
 
+
+      app.post('/users', async(req,res) =>{
+          const user = req.body;
+          const result = await userCollection.insertOne(user)
+          console.log(req.body);
+          console.log(result);
+          res.json(result);
+      })
+
+      app.put('/users/admin', async(req,res) =>{
+        const user = req.body;
+        const filter = {email: user.email}
+        const updateDoc = {$set: {role:'admin'}}
+        const result = await userCollection.updateOne(filter,updateDoc)
+        res.send(result);
+      })
+
+
+      app.get('/users/:email', async(req,res) =>{
+            const email = req.params.email;
+            const query = {email: email}
+            const result = await userCollection.findOne(query)
+            let isAdmin = (false)
+            if(result?.role === "admin")
+            {
+              isAdmin = true
+            }
+            res.json({admin: isAdmin})
+      })
+
+
+      app.post('/experience', async(req,res) =>{
+          const experience = req.body;
+          const result = await experienceCollection.insertOne(experience)
+          console.log(req.body)
+          console.log(result)
+          res.json(result);
+      })
+
+      app.get('/experience', async(req,res) =>{
+          const cursor = experienceCollection.find({})
+          const page = req.query.page;
+          const size = parseInt(req.query.size);
+          let result;
+          const count = await cursor.count()
+          if(page){
+            result = await cursor.skip(page*size).limit(size).toArray()
+          }
+          else{
+            result = await cursor.toArray()
+          }
+         
+          res.send({
+            count,
+            result
+            
+          });
+      })
+
+      app.get('/experience/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await experienceCollection.findOne(query)
+            res.send(result);
+      })
       
       
 
